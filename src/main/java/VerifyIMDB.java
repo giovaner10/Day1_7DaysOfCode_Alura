cd3ce0b0ffff737f3e16e65a741a525101a8b999
giovaner10 <giovanerr10@gmail.com>
2022-06-08 10:12:19 -0300

    Classe com metodo que fara a requisicao

---
+++ src/main/java/VerifyIMDB.java
@@ -0,0 +1,29 @@
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpRequest;
+
+public class VerifyIMDB {
+
+
+    public HttpRequest getRequest() {
+
+
+        HttpRequest request = null;
+
+        {
+            try {
+                request = HttpRequest
+                        .newBuilder()
+                        .uri(new URI("https://imdb-api.com/API/Search/k_12345678/leon%20the%20professional"))
+                        .GET()
+                        .build();
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        return request;
+
+    }
+}
